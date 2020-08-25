b4ef4c526373711628a9f8cffe3c99c8799a37aa
jkairoy <jkairoy@gmail.com>
2020-08-24 21:03:21 -0400

    closer

--- assets/js/header.js
+++ assets/js/header.js
@@ -9,6 +9,15 @@ $('.header__hamburger').on("click", function () {
         }, 500);
         $(".header__hamburger").toggleClass('open');
         $(".header__vertical").toggleClass('open');
-
     }
 });
+
+$(window).on("scroll", function (e) {
+    if ($("#s3").offset().top - $(window).scrollTop() <= 0 ) {
+        $("header").css({backgroundColor: "rgb(248,221,100)"})
+    } else if ($("#s2").offset().top - $(window).scrollTop() <= 0 ) {
+        $("header").css({backgroundColor: "rgb(175,216,208,0.5)"})
+    } else {
+        $("header").css({backgroundColor: "rgb(255,255,255,0.5)"})
+    }
+})
