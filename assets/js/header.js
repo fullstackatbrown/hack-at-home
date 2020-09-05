ecb1812a176cbbb55db51446170e87ed3dfbb30e
jkairoy <jkairoy@gmail.com>
2020-09-04 21:25:50 -0400

    header fix

--- assets/js/header.js
+++ assets/js/header.js
@@ -13,11 +13,12 @@ $('.header__hamburger').on("click", function () {
 });
 
 function adaptHeader() {
-    if ($("#s4").offset().top - $(window).scrollTop() <= 0 ) {
+    console.log($("#s4").offset().top - $(window).scrollTop() )
+    if ($("#s4").offset().top - $(window).scrollTop() <= 2 ) {
         $("header").css({backgroundColor: "rgb(248,221,100,1)"})
-    } else if ($("#s3").offset().top - $(window).scrollTop() <= 0 ) {
+    } else if ($("#s3").offset().top - $(window).scrollTop() <= 2 ) {
         $("header").css({backgroundColor: "rgb(145,183,112,1)"})
-    } else if ($("#s2").offset().top - $(window).scrollTop() <= 0 ) {
+    } else if ($("#s2").offset().top - $(window).scrollTop() <= 2 ) {
         $("header").css({backgroundColor: "rgb(175,216,208,1)"})
     } else {
         $("header").css({backgroundColor: "rgb(255,255,255,1)"})
